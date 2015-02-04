f63568b4e58b7683591918fbf1963c8962f1bb62
Robert Hutchinson <robert.hutchinson@gmail.com>
2015-02-04 15:05:33 -0500

    Testing that rolling a 0 will clear out temp score

--- game_bacon_bits.rb
+++ game_bacon_bits.rb
@@ -13,9 +13,23 @@ class Game
 end
 
 class Pig < Game
-  
+  attr_reader :temp_score
+
   def initialize maxscore=nil
     super (maxscore)
+    @temp_score = 0
   end
 
+  def one_roll
+    unless roll == 1
+      @temp_score += roll
+    else
+      @temp_score = 0
+    end
+    
+    @temp_score
+  end
+
+  def bank
+  end
 end
--- test_game.rb
+++ test_game.rb
@@ -6,20 +6,20 @@ require "./game_bacon_bits"
 SimpleCov.start
 
 class PigGameTests < MiniTest::Test
-  def test_games_know_max_score
-    g = Game.new 100
+  def test_pig_knows_max_score
     p = Pig.new 100
-    
-    assert_equal g.maxscore, 100
     assert_equal p.maxscore, 100
   end
 
-  def test_games_can_roll_dice
-    g = Game.new 
+  def test_pig_can_roll_dice
     p = Pig.new 
-
-    assert (1..6).include?(g.roll)
     assert (1..6).include?(p.roll)
+  end
+
+  def test_turn_score_can_return_0
+    p = Pig.new
+   
+    assert p.one_roll > 1 ||  0
   end
 
 end
