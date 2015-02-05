e459b5e9f3e17bc6907db0997f599e68c9e916a2
Robert Hutchinson <robert.hutchinson@gmail.com>
2015-02-04 19:12:04 -0500

    Struggling toward a working Pig implementation

--- game_bacon_bits.rb
+++ game_bacon_bits.rb
@@ -1,38 +1,59 @@
 require 'pry'
 
 class Game
-  attr_reader :maxscore
+  attr_reader :maxscore, :scores, :winner
 
-  def initialize maxscore=nil
-    @maxscore = maxscore || 100
+  def initialize maxscore=nil, players=2
+    @maxscore = maxscore || 10
+    @scores = Array.new(players, 0)
+    @winner = false
   end
 
   def die_roll
     rand(1..6)
   end
+
+  def winner
+   (0...@scores.length).map{|x| if @scores[x]>100
+    @winner = true end}
+  end
+
 end
 
 class Pig < Game
-  attr_reader :temp_score
+  attr_reader :temp_score, :roll
 
-  def initialize maxscore=nil
-    super (maxscore)
+  def initialize
+    super
     @temp_score = 0
+    
   end
 
-  def one_roll
-    roll = die_roll
-    unless roll == 1
-      @temp_score += roll
-      puts "You rolled a #{roll}. 
-Your score for the turn is #{@temp_score}"
-    else
+
+  def turn roll
+      # roll = die_roll
+      unless roll == 1
+        @temp_score += roll
+        roll
+#         puts "You rolled a #{roll}. 
+# Your score for the turn is #{@temp_score}. Would
+# you like to roll again?(y/n?)\n"
+        # if gets.chomp == "n"
+        #    @scores[player_num] += @temp_score
+        #    @temp_score = 0
+      else
+        @temp_score = 0
+        1
+#       puts "Sorry! You rolled a 1 and lost your points!
+# Your score is still #{@scores[player_num]}.\n"
+      end
+  end
+
+  def bank player_num = 0
+      @scores[player_num] += @temp_score
+      #puts "Your score is now #{@scores[player_num]}."
       @temp_score = 0
-      puts "Sorry! You rolled a 1 and lost your points!"
-    end
-    @temp_score
   end
 
-  def bank
-  end
+ 
 end
--- test_game.rb
+++ test_game.rb
@@ -1,14 +1,61 @@
 require 'simplecov'
+SimpleCov.start
+
 require 'pry'
 require 'minitest/autorun'
 require "./game_bacon_bits"
 
-SimpleCov.start
+
+
+# p = Pig.new
+
+# players = Array.new(2, "")
+
+# until p.winner
+#   (0...players.length).map{|x|
+#   loop do
+#     puts "You rolled a #{p.turn(p.roll)}. Your score for the turn is now #{p.temp_score}."
+#     if p.temp_score != 0
+#       puts "Would you like to roll again?"
+#       if gets.chomp == "n"
+#         p.bank(x)
+#         puts "\nYour score is now #{p.scores[x]}.\n"
+#         sleep(1)
+#         "-------------------------------------------"
+#         break
+#       end
+#     else
+#       break
+#     end
+#   end
+#   }
+# end
+
+
+# while winner == false  
+# (0...players.length).map{|x| loop do
+#   p.turn(players.index[x])
+#   puts "You rolled a #{roll}. Your score
+# for the turn is #{@turn_score}. Would you like
+# to roll again?"
+#   if gets.chomp == "n"
+#     p.bank(players.index[x])
+#   else
+
+# end
+
+# puts "\n\nFinal Scoreboard:\n"
+# (0...players.length).map {|x| puts "#{players[x]}: #{@scores[x]}"}
+
+
+  
+
+
 
 class PigGameTests < MiniTest::Test
   def test_pig_knows_max_score
-    p = Pig.new 100
-    assert_equal p.maxscore, 100
+    p = Pig.new
+    assert_equal p.maxscore, 10
   end
 
   def test_pig_can_roll_dice
@@ -16,10 +63,28 @@ class PigGameTests < MiniTest::Test
     assert (1..6).include?(p.die_roll)
   end
 
-  def test_turn_score_can_return_0
+  def test_non_ones_give_points
     p = Pig.new
-    assert p.one_roll > 1 ||  0
+    assert p.turn(2) > 0
   end
 
+  def test_ones_give_zero_points
+    p = Pig.new
+    p.turn(1)
+    assert_equal 0, p.temp_score
+  end
 
+  def test_players_can_bank_scores
+    p = Pig.new
+    p.turn(5)
+    p.bank
+    assert_equal p.scores.first, 5
+  end
+
+  def test_players_can_win
+    p=Pig.new
+    p.turn(105)
+    p.bank
+    assert p.winner
+  end
 end
